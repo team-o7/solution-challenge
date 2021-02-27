6e0830ccb17870f8b116ac932b57aacacd3f88ee
prakash-cr7 <prakashkumarraj1@gmail.com>
2021-02-27 09:34:46 +0530

    Backed function for updating topics to user

--- backend/functions/.eslintrc.js
+++ backend/functions/.eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
   ],
   rules: {
     quotes: ["error", "double"],
+    indent: "off",
   },
 };
--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,10 +1,26 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
+admin.firestore().settings({ ignoreUndefinedProperties: true });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  console.log("Hello ");
-  response.send("Hello from Firebase!");
-});
+export const onTopicCreate = functions.firestore
+  .document("/topics/{topicId}")
+  .onCreate(async (snap, context) => {
+    // Grab the current value of what was written to Firestore.
+    const original = snap.data();
+    // const topicId = context.params.topicId;
+    const creator = original.creator;
+    const d = await admin
+      .firestore()
+      .collection("users")
+      .where("uid", "==", creator)
+      .get();
+    // const s = original.id;
+
+    d.forEach((doc) => {
+      doc.ref.update({
+        topics: admin.firestore.FieldValue.arrayUnion(context.params.topicId),
+      });
+    });
+  });
